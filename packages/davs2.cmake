463df06dfc769b5b67ada43afd4d0939584ae16f
Sle2p <www.766111444@foxmail.com>
2026-07-30 21:16:12 +0800

    Update davs2.cmake

--- packages/davs2.cmake
+++ packages/davs2.cmake
@@ -2,18 +2,30 @@ if(${TARGET_CPU} MATCHES "i686")
     set(disable_asm "--disable-asm")
 endif()
 
+# Generate a configure wrapper script to avoid CMake argument splitting issues
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/davs2-configure.sh
+"#!/bin/bash
+set -e
+cd ${SOURCE_LOCATION}/build/linux
+export CC=${TARGET_ARCH}-gcc
+export CXX=${TARGET_ARCH}-g++
+./configure \
+    --host=${TARGET_ARCH} \
+    --cross-prefix=${TARGET_ARCH}- \
+    --prefix=${MINGW_INSTALL_PREFIX} \
+    --disable-cli \
+    --bit-depth=10 \
+    ${disable_asm}
+")
+file(CHMOD ${CMAKE_CURRENT_BINARY_DIR}/davs2-configure.sh
+    PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+
 ExternalProject_Add(davs2
     GIT_REPOSITORY https://github.com/saindriches/davs2.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} cd <SOURCE_DIR>/build/linux && env CC=${TARGET_ARCH}-g++ CXX=${TARGET_ARCH}-g++ ./configure
-        --host=${TARGET_ARCH}
-        --cross-prefix=${TARGET_ARCH}-
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --disable-cli
-        --bit-depth=10
-        ${disable_asm}
+    CONFIGURE_COMMAND ${EXEC} ${CMAKE_CURRENT_BINARY_DIR}/davs2-configure.sh
     BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/build/linux CC=${TARGET_ARCH}-gcc CXX=${TARGET_ARCH}-g++
     INSTALL_COMMAND ${MAKE} -C <SOURCE_DIR>/build/linux install CC=${TARGET_ARCH}-gcc CXX=${TARGET_ARCH}-g++
     BUILD_IN_SOURCE 1
