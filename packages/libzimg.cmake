dc587f27e458f8fd1cb028bed3b772999beadb88
Sle2p <www.766111444@foxmail.com>
2026-07-30 20:11:20 +0800

    Update libzimg.cmake

--- packages/libzimg.cmake
+++ packages/libzimg.cmake
@@ -8,10 +8,11 @@ ExternalProject_Add(libzimg
     GIT_SUBMODULES ""
     PATCH_COMMAND ${EXEC} sed -i "s/Windows.h/windows.h/g" src/zimg/common/arm/cpuinfo_arm.cpp
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} rm -rf <SOURCE_DIR>/graphengine && ln -s ${src_graphengine} <SOURCE_DIR>/graphengine && cd <SOURCE_DIR> && NOCONFIGURE=1 ./autogen.sh && CONF=1 ./configure
+    CONFIGURE_COMMAND ${EXEC} rm -rf <SOURCE_DIR>/graphengine && ln -sf ${src_graphengine} <SOURCE_DIR>/graphengine && NOCONFIGURE=1 <SOURCE_DIR>/autogen.sh && CONF=1 <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --prefix=${MINGW_INSTALL_PREFIX}
         --disable-shared
+        && touch <SOURCE_DIR>/Makefile
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
             COMMAND bash -c "git -C ${src_graphengine} clean -dfx"
