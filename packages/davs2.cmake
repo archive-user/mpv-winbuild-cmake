00f92321df4ba71d9a74c57acc4885231fa2397c
Sle2p <www.766111444@foxmail.com>
2026-08-01 16:01:34 +0800

    Update davs2.cmake

--- packages/davs2.cmake
+++ packages/davs2.cmake
@@ -1,43 +1,24 @@
-function(build_davs2 TARGET_CPU TARGET_ARCH MINGW_INSTALL_PREFIX)
-    set(SYSROOT "/usr/${TARGET_ARCH}")
-    
-    if(${TARGET_CPU} MATCHES "i686")
-        set(disable_asm "--disable-asm")
-    endif()
-    
-    ExternalProject_Add(davs2
-        GIT_REPOSITORY https://github.com/saindiches/davs2.git
-        SOURCE_DIR ${SOURCE_LOCATION}
-        GIT_CLONE_FLAGS "--filter=tree:0"
-        UPDATE_COMMAND ""
-        CONFIGURE_COMMAND 
-            ${EXEC} bash -c "
-                cd ${SOURCE_LOCATION}/build/linux && 
-                CC=${TARGET_ARCH}-gcc 
-                CXX=${TARGET_ARCH}-g++ 
-                CPPFLAGS='-I${SYSROOT}/include -I${MINGW_INSTALL_PREFIX}/include' 
-                LDFLAGS='-L${SYSROOT}/lib -L${MINGW_INSTALL_PREFIX}/lib' 
-                ./configure 
-                    --host=${TARGET_ARCH} 
-                    --cross-prefix=${TARGET_CPU}-w64-mingw32- 
-                    --prefix=${MINGW_INSTALL_PREFIX} 
-                    --disable-cli 
-                    --bit-depth=10 
-                    ${disable_asm}
-            "
-        BUILD_COMMAND 
-            ${EXEC} bash -c "
-                cd ${SOURCE_LOCATION}/build/linux && 
-                make CC=${TARGET_ARCH}-gcc 
-                     CXX=${TARGET_ARCH}-g++ 
-                     CPPFLAGS='-I${SYSROOT}/include -I${MINGW_INSTALL_PREFIX}/include' 
-                     LDFLAGS='-L${SYSROOT}/lib -L${MINGW_INSTALL_PREFIX}/lib'
-            "
-        INSTALL_COMMAND 
-            ${EXEC} bash -c "
-                cd ${SOURCE_LOCATION}/build/linux && 
-                make install
-            "
-        BUILD_IN_SOURCE 1
-    )
-endfunction()
+if(${TARGET_CPU} MATCHES "i686")
+    set(disable_asm "--disable-asm")
+endif()
+
+ExternalProject_Add(davs2
+    GIT_REPOSITORY https://github.com/saindriches/davs2.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_CLONE_FLAGS "--filter=tree:0"
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${EXEC} cd <SOURCE_DIR>/build/linux && CONF=1 ./configure
+        --host=${TARGET_ARCH}
+        --cross-prefix=${TARGET_CPU}-w64-mingw32-
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --disable-cli
+        --bit-depth=10
+        ${disable_asm}
+    BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/build/linux
+    INSTALL_COMMAND ${MAKE} -C <SOURCE_DIR>/build/linux install
+    BUILD_IN_SOURCE 1
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(davs2)
+cleanup(davs2 install)
